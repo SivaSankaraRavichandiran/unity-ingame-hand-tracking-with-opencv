608a6e25afe7d913d1fe5dff6a09b31866927a9c
siva <sivasankaran.s1231811@gmail.com>
2024-03-21 15:01:51 +0530

    base of capturing cam feed

--- test.py
+++ test.py
@@ -1,16 +1,39 @@
 import cv2
+from cvzone.HandTrackingModule import HandDetector
+import socket
 
-
-#cam frame size
 height , width = 720 , 1280
-
-# my cam
+# My WebCam
 cap = cv2.VideoCapture(0)
-cap.set(3, width)
-cap.set(4, height)
+cap.set(3,width)
+cap.set(4,height)
+
+# hand detection
+detec1 = HandDetector(maxHands=2,detectionCon=0.7)
+
+# communication
+sockunity = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+serverPortAddress = ("127.0.0.1" , 5052 )
 
 while True:
-    #geting fram from cam
-    success , img = cap.read()
-    cv2.imshow('img',img)
+    # Get frame from the webcam
+    success, img = cap.read()
+    # hands
+    hands, img = detec1.findHands(img)
+
+    data = []
+
+    #landmark values (x,y,z)*21  -there are 12 landmarks
+    if hands:
+        #get the first hand detected
+        hand = hands[0]
+        #get the landmark list
+        lmList = hand['lmList']
+        #print(lmList)
+        for lm in lmList:
+            data.extend([lm[0],height - lm[1],lm[2]])
+        #print(data)
+        sockunity.sendto(str.encode(str(data)), serverPortAddress)
+
+    cv2.imshow('image',img)
     cv2.waitKey(1)
