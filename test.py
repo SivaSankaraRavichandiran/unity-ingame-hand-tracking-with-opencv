c219e5578a035fcac5fc9bb357f65ff0699232dc
siva <sivasankaran.s1231811@gmail.com>
2024-03-21 18:29:22 +0530

    added the feature to keep the hand center

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ cap.set(3,width)
 cap.set(4,height)
 
 # hand detection
-detec1 = HandDetector(maxHands=2,detectionCon=0.7)
+detec1 = HandDetector(maxHands=2,detectionCon=.75)
 
 # communication
 sockunity = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -23,7 +23,7 @@ while True:
 
     data = []
 
-    #landmark values (x,y,z)*21  -there are 12 landmarks
+    #landmark values (x,y,z)*21  -there are 21 landmarks
     if hands:
         #get the first hand detected
         hand = hands[0]
@@ -34,6 +34,6 @@ while True:
             data.extend([lm[0],height - lm[1],lm[2]])
         #print(data)
         sockunity.sendto(str.encode(str(data)), serverPortAddress)
-
+    img = cv2.resize(img, (0,0),None, 0.5, 0.5)
     cv2.imshow('image',img)
     cv2.waitKey(1)
